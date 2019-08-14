37f41e2ac3b6482ee3fd1a95b4d947aa2765e6b7
Ariel <arielord@buffalo.edu>
2019-08-14 02:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city){
+  let result = city === "NYC" ? "Ok, sounds good.":"No go.";
+  return result;
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  }
+  els
+}
